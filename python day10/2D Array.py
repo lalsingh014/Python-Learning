7a50980d88f5625fdd5c6db9bae12ed5f5f1a1e0
lalsingh014 <lalsinghsodhakanod@gmail.com>
2026-02-05 19:06:52 +0530

    Day 10 Numpy Basics

---
+++ python day10/2D Array.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+mat = np.array([[9, 2, 6], [5, 3, 8], [83, 567, 34]])
+
+print(mat)
+print(mat.shape)
---
+++ python day10/Generate Numeric Ranges.py
@@ -0,0 +1,4 @@
+import numpy as np 
+
+print(np.arange(3, 41, 4))
+print(np.linspace(2, 8, 4))
